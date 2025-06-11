ae7f92a338c2ff7f7836a0b08ea2f94b5449b95c
Bennett <Rachel.L.Bennett-1@ou.edu>
2025-06-11 16:08:34 -0500

    Added group membership as an argument

--- sksurv/ensemble/forest.py
+++ sksurv/ensemble/forest.py
@@ -837,7 +837,7 @@ class FairRandomSurvivalForest(SurvivalAnalysisMixin, _BaseSurvivalForest):
         low_memory=False,
     ):
         super().__init__(
-            estimator=SurvivalTree(),
+            estimator=FairSurvivalTree(),
             n_estimators=n_estimators,
             estimator_params=(
                 "max_depth",
@@ -866,6 +866,123 @@ class FairRandomSurvivalForest(SurvivalAnalysisMixin, _BaseSurvivalForest):
         self.max_leaf_nodes = max_leaf_nodes
         self.low_memory = low_memory
 
+    def fit(self, X, y, group, sample_weight=None):
+        """Build a forest of survival trees from the training set (X, y).
+
+        Parameters
+        ----------
+        X : array-like, shape = (n_samples, n_features)
+            Data matrix
+
+        y : structured array, shape = (n_samples,)
+            A structured array containing the binary event indicator
+            as first field, and time of event or time of censoring as
+            second field.
+
+        Returns
+        -------
+        self
+        """
+        self._validate_params()
+
+        X = validate_data(self, X, dtype=DTYPE, accept_sparse="csc", ensure_min_samples=2, ensure_all_finite=False)
+        event, time = check_array_survival(X, y)
+
+        # _compute_missing_values_in_feature_mask checks if X has missing values and
+        # will raise an error if the underlying tree base estimator can't handle missing
+        # values.
+        estimator = type(self.estimator)()
+        missing_values_in_feature_mask = estimator._compute_missing_values_in_feature_mask(
+            X, estimator_name=self.__class__.__name__
+        )
+
+        self._n_samples, self.n_features_in_ = X.shape
+        time = time.astype(np.float64)
+        self.unique_times_, self.is_event_time_ = get_unique_times(time, event)
+        self.n_outputs_ = self.unique_times_.shape[0]
+
+        y_numeric = np.empty((X.shape[0], 2), dtype=np.float64)
+        y_numeric[:, 0] = time
+        y_numeric[:, 1] = event.astype(np.float64)
+
+        # Get bootstrap sample size
+        if not self.bootstrap and self.max_samples is not None:  # pylint: disable=no-else-raise
+            raise ValueError(
+                "`max_sample` cannot be set if `bootstrap=False`. "
+                "Either switch to `bootstrap=True` or set "
+                "`max_sample=None`."
+            )
+        elif self.bootstrap:
+            n_samples_bootstrap = _get_n_samples_bootstrap(n_samples=X.shape[0], max_samples=self.max_samples)
+        else:
+            n_samples_bootstrap = None
+
+        self._n_samples_bootstrap = n_samples_bootstrap
+
+        # Check parameters
+        self._validate_estimator()
+
+        if not self.bootstrap and self.oob_score:
+            raise ValueError("Out of bag estimation only available if bootstrap=True")
+
+        random_state = check_random_state(self.random_state)
+
+        if not self.warm_start or not hasattr(self, "estimators_"):
+            # Free allocated memory, if any
+            self.estimators_ = []
+
+        n_more_estimators = self.n_estimators - len(self.estimators_)
+
+        if n_more_estimators < 0:  # pylint: disable=no-else-raise
+            raise ValueError(
+                f"n_estimators={self.n_estimators} must be larger or equal to "
+                f"len(estimators_)={len(self.estimators_)} when warm_start==True"
+            )
+        elif n_more_estimators == 0:
+            warnings.warn("Warm-start fitting without increasing n_estimators does not fit new trees.", stacklevel=2)
+        else:
+            if self.warm_start and len(self.estimators_) > 0:
+                # We draw from the random state to get the random state we
+                # would have got if we hadn't used a warm_start.
+                random_state.randint(MAX_INT, size=len(self.estimators_))
+
+            trees = [self._make_estimator(append=False, random_state=random_state) for i in range(n_more_estimators)]
+
+            y_tree = (
+                y_numeric,
+                self.unique_times_,
+                self.is_event_time_,
+            )
+            # Parallel loop: we prefer the threading backend as the Cython code
+            # for fitting the trees is internally releasing the Python GIL
+            # making threading more efficient than multiprocessing in
+            # that case. However, for joblib 0.12+ we respect any
+            # parallel_backend contexts set at a higher level,
+            # since correctness does not rely on using threads.
+            trees = Parallel(n_jobs=self.n_jobs, verbose=self.verbose, prefer="threads")(
+                delayed(_parallel_build_trees)(
+                    t,
+                    self.bootstrap,
+                    X,
+                    y_tree,
+                    sample_weight,
+                    i,
+                    len(trees),
+                    verbose=self.verbose,
+                    n_samples_bootstrap=n_samples_bootstrap,
+                    missing_values_in_feature_mask=missing_values_in_feature_mask,
+                )
+                for i, t in enumerate(trees)
+            )
+
+            # Collect newly grown trees
+            self.estimators_.extend(trees)
+
+        if self.oob_score:
+            self._set_oob_score_and_attributes(X, (event, time))
+
+        return self
+    
     def predict_cumulative_hazard_function(self, X, return_array=False):
         """Predict cumulative hazard function.
 
